53f557bab18fa336dbb518ed205d6d58cb56142d
fannychan <fannt@kth.se>
2017-12-15 15:23:09 +0100

    added window in canvas

---
+++ Assets/Earth/Scripts/PlanetAudioScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetAudioScript : MonoBehaviour {
+
+	public AudioClip audio;
+	public GameObject trigger;
+	// Use this for initialization
+	void Start () {
+		
+		GetComponent<AudioSource> ().playOnAwake = false;
+		GetComponent<AudioSource> ().clip = audio;
+	}
+
+	void OnCollisionEnter(Collider collision)
+	{
+		if (collision.tag == "MainCamera") {
+
+			trigger.SetActive (true);
+			GetComponent<AudioSource> ().Play ();
+		}
+	}
+}
